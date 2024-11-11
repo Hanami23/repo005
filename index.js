026ec187dd4f2053e93610552cf6d38c6dadad85
Hanami23 <165859450+Hanami23@users.noreply.github.com>
2024-11-11 16:57:32 -0300

    Test Data Json

---
+++ index.js
@@ -0,0 +1,9 @@
+const jsonServe = require("json-server");
+const server = jsonServe.create;
+const router = jsonServe.router("almacen.json");
+const middleware = jsonServe.defaults();
+const port = process.env.PORT || 10000;
+
+server.use(middleware);
+server.use(router);
+server.listen(port);
